015966c7e0d72c2ef47bba8bb93a3f34b6afb391
Rafael Jamur <jamur@ufpr.br>
2014-01-28 09:17:48 -0200

    Parte 4 completa - Já com Generic Views

--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
-from polls.models import Choice, Poll as Question
+from polls.models import Choice, Poll
+from django.views import generic
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published polls."""
+        return Poll.objects.order_by('-pub_date')[:5]
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     #template = loader.get_template('polls/index.html')
@@ -17,6 +27,11 @@ def index(request):
     #return HttpResponse(template.render(context))
     return render(request, 'polls/index.html', context)
 
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+
 def detail(request, question_id):
     #return HttpResponse("You're looking at question %s." % question_id)
     #try:
@@ -26,18 +41,24 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
+
+
 def results(request, question_id):
     poll = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'poll': poll})
 
-def vote(request, question_id):
-    p = get_object_or_404(Question, pk=question_id)
+def vote(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
     try:
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay teh poll voting form.
         return render(request, 'polls/detail.html', {
-            'question': p,
+            'poll': p,
             'error_message': "You didn't select a choice.",
         })
     else:
